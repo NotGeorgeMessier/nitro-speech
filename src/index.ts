e667937333e654ee0628382e0cba31c2b84897e0
Andrei Kologreev <andrei_kologreev@belk.com>
2025-12-20 19:07:17 +0400

    fuchyeah

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-// TODO: Export all HybridObjects here for the user
+import { NitroModules } from 'react-native-nitro-modules'
+import type { Math } from './specs/Example.nitro'
+
+export const NitroSpeech = NitroModules.createHybridObject<Math>('Math')
--- src/specs/Example.nitro.ts
+++ src/specs/Example.nitro.ts
@@ -1,7 +1,7 @@
 import { type HybridObject } from 'react-native-nitro-modules'
 
 export interface Math extends HybridObject<{
-  ios: 'swift',
+  ios: 'swift'
   android: 'kotlin'
 }> {
   add(a: number, b: number): number
